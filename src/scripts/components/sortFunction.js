83eee477733608cf62d88567d7f5adb0b7828668
Daniel Sitarek <deltasierra777@gmail.com>
2021-09-11 02:49:17 -0500

    bug fix

--- src/scripts/components/sortFunction.js
+++ src/scripts/components/sortFunction.js
@@ -4,9 +4,9 @@ const sortBy = (arr) => {
   if (sortMethod === 'A-Z') {
     sortedTerms = (arr).sort((a, b) => ((a.title > b.title) && 1) || -1);
   } if (sortMethod === 'newest') {
-    sortedTerms = (arr).sort((a, b) => a.timestamp.localeCompare(b.timestamp));
-  } if (sortMethod === 'oldest') {
     sortedTerms = (arr).sort((a, b) => b.timestamp.localeCompare(a.timestamp));
+  } if (sortMethod === 'oldest') {
+    sortedTerms = (arr).sort((a, b) => a.timestamp.localeCompare(b.timestamp));
   }
   return sortedTerms;
 };
